97e68a5faec719696584fc3379086bcc0cfbf456
drinko-dr <54978399+drinko-dr@users.noreply.github.com>
2019-09-25 23:40:44 +0300

    Add files via upload
    v2

---
+++ v2/solver.c
@@ -0,0 +1,49 @@
+//
+// Created by Leto Vania on 20/09/2019.
+//
+
+#include "fillit.h"
+
+int 	solver(int **map, t_shape *tet, int size)
+{
+	int 	row;
+	int		col;
+
+	row = 0;
+	col = 0;
+	//какая-то хуета, забей
+	// if (tet->coord[7] == ((size * size) - 1) || tet->coord[6] == ((size * size) - 2))
+	// 	return (0);
+	if (tet->next == NULL)
+		return (1);
+	while (row < size) // заменить на start_row
+	{
+		col = 0;
+		while (col < size) // заменит start_col
+		{
+			if (check_place(map, tet, row, col, size)) {
+				tetramino_set(map, tet, row, col);
+				// print_shape(map, size + 1);
+				// printf("\n");
+				if (solver(map, tet->next, size))
+					return (1);
+				tetramino_unset(map, tet, row, col);
+			}
+			col++;
+		}
+		row++;
+	}
+	
+	// if (tet->next)
+	// 	return (0);
+	return (0); // что нужно вернуть ?
+}
+
+
+// для квадрата размером size_square
+// для каждой i,j ячейки попробовать установить фигуру
+// 		если фигура выходит за границы квадрата или наезжает на другую фигуру сдвинуть фигуру ввправо
+//		если не получается сдвинуть вправо сдвинуть вниз
+//	повторять пока первая фигура не окажеться в правом верхнем углу
+//		если отсутствует площадь куда можно поставить фигуру, увеличение размера квадрата и начать сначала
+//
---
+++ v2/structe.c
@@ -0,0 +1,34 @@
+
+#include "fillit.h"
+
+t_shape		*creat_new_list(void)
+{
+	t_shape		*node;
+
+	if (!(node = (t_shape *)malloc(sizeof(t_shape))))
+		return (NULL);
+	node->next = NULL;
+	return (node);
+}
+
+void		del_list(t_shape **shape)
+{
+	if (*shape && (*shape)->next)
+		del_list(&(*shape)->next);
+	free(*shape);
+	*shape = NULL;
+}
+
+int		read_shape(char *buf, int ret, t_shape **shape, int count)
+{
+	if(!check_tetramino(buf,ret))
+		return (0);
+	else
+	{
+		save_coord(buf, shape, count);
+		move_shape(shape);
+		(*shape)->next = creat_new_list();
+		*shape = (*shape)->next;
+	}
+	return (1);
+}
